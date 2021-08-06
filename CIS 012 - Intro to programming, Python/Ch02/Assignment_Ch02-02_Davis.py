a00d0692d70468035c371f52e3078cfb63cf6bf1
davis-andy <davis.andy1021@gmail.com>
2021-08-05 18:25:34 -0700

    folder renames

--- CIS 012 - Intro to programming, Python/Ch02/Assignment_Ch02-02_Davis.py
+++
@@ -1,4 +0,0 @@
-firstName = input("Enter your first name:")
-studyHours = input("Enter your expected study hours on Saturday:")
-
-print ("\nHello, " + firstName + "!" + "\n" + "On Saturday, you need to study " + studyHours + " hours for the exam.")
--- CIS 012 - Intro to programming, Python/Ch04/Assignment_Ch04-03_Davis.py
+++
@@ -1,9 +0,0 @@
-import RPSMod
-
-player = RPSMod.collectStringInput()
-computer = RPSMod.generateIntegerValue()
-computer_choice = RPSMod.convertIntegerToRPS(computer)
-print()
-print("Computer's selection:",computer_choice)
-print()
-RPSMod.evaluateWinning(player, computer_choice)
