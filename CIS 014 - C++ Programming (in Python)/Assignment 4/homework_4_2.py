11a8f2c6b01ef1b9774af20c15974fe6dce1c2fa
davis-andy <davis.andy1021@gmail.com>
2021-08-07 21:54:52 -0700

    Update homework_4_2.py

--- CIS 014 - C++ Programming (in Python)/Assignment 4/homework_4_2.py
+++ CIS 014 - C++ Programming (in Python)/Assignment 4/homework_4_2.py
@@ -1,11 +1,30 @@
 def deduplicate(inp: str) -> str:
+    i = 0
+    j = 0
 
+    if len(inp) == 1:
+        return inp
 
+    while i < len(inp)-1:
+        while j < len(inp)-1:
+            if j + 1 > len(inp) - 1:
+                break
+            elif inp[j] == inp[j+1]:
+                inp = inp[:j] + inp[j + 2:]
+            else:
+                j += 1
 
+        if i + 1 > len(inp) - 1:
+            break
+        elif inp[i] == inp[i + 1]:
+            inp = inp[:i] + inp[i + 2:]
+        else:
+            i += 1
 
+    if inp == '':
+        inp = 'Empty'
 
-
-
+    return inp
 
 
 def main():
@@ -14,3 +33,6 @@ def main():
     print(deduplicate("ABBA"))  # should output "Empty"
     print(deduplicate("AAA"))  # "A"
     print(deduplicate("AKA"))  # "AKA" because there is no consecutive pair.
+
+
+main()
